b5987c065949f9e3364d4e42959fc93b6d905b51
Joseph Castillo <joey.castillo@acmedev.com>
2026-03-23 16:00:54 +0800

    bump to v1.8.4

--- metal/Go.Li.bat
+++ metal/Go.Li.bat
@@ -1,6 +1,6 @@
 @echo off
 rem metal.Li — Lithium. OS diet.
-rem v1.8.3 — 2026-03-23
+rem v1.8.4 — 2026-03-23
 rem curl -fsSL https://joeycastillo.us/metal/Go.Li.bat -o %TEMP%\Go.Li.bat && %TEMP%\Go.Li.bat
 rem Zero dependencies. Just run as Administrator.
 rem Apply all at once or choose step by step.
@@ -10,7 +10,7 @@ setlocal enabledelayedexpansion
 
 echo.
 echo   metal.Li — Lithium. OS diet.
-echo   v1.8.3 — 2026-03-23
+echo   v1.8.4 — 2026-03-23
 echo   Strip the fat. Dark mode. Full power.
 echo.
 
